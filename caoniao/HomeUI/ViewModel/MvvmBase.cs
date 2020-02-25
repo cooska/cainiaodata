54a24306404ab0f1aaf19dff0b59ca76fd546959
甄立 <zhenli@bakclass.com>
2020-02-25 09:49:38 +0800

    一些初始文件

---
+++ caoniao/HomeUI/ViewModel/MvvmBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace HomeUI.ViewModel
+{
+    class MvvmBase : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged 成员
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        virtual internal protected void OnPropertyChanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+    }
+}
